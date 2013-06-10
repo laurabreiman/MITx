72ea65bdcd116c60cd1ccbe33356626f715efd02
OpenShift guest <breimanlaura@gmail.com>
2013-06-10 16:27:13 -0400

    calculator with final changes

--- firstDay.js
+++ firstDay.js
@@ -1,5 +1,5 @@
 function calculate(text){
-    var pattern = /\d+|\+|\-|\*|\/|\(|\)/g;
+    var pattern = /\d*\.?\d+|\+|\-|\*|\/|\(|\)|\w*\(/g;
     var tokens = text.match(pattern);
     try{
         var val = evaluate(tokens);
@@ -24,23 +24,35 @@ function setup_calc(div){
 
 function read_operand(tokens){
     try{
-        var num = tokens[0];
-        tokens.shift();
+        var num = tokens.shift();
         if (num == '('){
-            var index = tokens.indexOf(')');
-            
-            if(index <0){
-                throw "mismatched delimiters";
-            }
-            
-            else{
-                num = evaluate(tokens.slice(0,index));
-                tokens = tokens.slice(index,tokens.length);
-            }
+            num = evaluate(tokens);
+            if(tokens.shift() != ')') throw "mismatched";
             return num;
         }
+        else if(num == '-'){
+            var next = tokens.shift();
+            tokens.unshift('(','0','-',next,')');
+            num = read_operand(tokens);
+            return num;
+        }
+        else if(num == "sin("){
+            num = Math.sin(evaluate(tokens));
+            if(tokens.shift() != ')') throw "mismatched";
+            return num;
+        }
+        else if(num == "log("){
+            num = Math.log(evaluate(tokens));
+            if(tokens.shift() != ')') throw "mismatched";
+            return num; 
+        }
+        else if(num == "sqrt("){
+            num = Math.sqrt(evaluate(tokens));
+            if(tokens.shift() != ')') throw "mismatched";
+            return num; 
+        }
         else{
-            num = parseInt(num,10);
+            num = parseFloat(num,10);
             if(isNaN(num)) throw "number expected";
             return num;
         }
@@ -52,28 +64,44 @@ function read_operand(tokens){
 
 function evaluate(tokens){
         if(tokens.length === 0) throw "missing operand";
-        var value = read_operand(tokens);
+        var value = read_term(tokens);
         var operator = 0;
         var temp = 0;
         while(tokens.length > 0){
-            operator = tokens[0];
-            tokens.shift();
+            if(tokens[0] == ')') return value;
+            operator = tokens.shift();
             if(['+','-','*','/'].indexOf(operator) < 0) throw "unrecognized operator";
             if(tokens.length === 0) throw "missing operand";
-            temp = read_operand(tokens);
+            temp = read_term(tokens);
             if(operator === '+'){
                 value = value+temp;
             } 
             if(operator === '-'){
                 value = value-temp;
             }
-            if(operator === '*'){
-                value = value*temp;
-            }
-            if(operator === '/'){
-                value = value/temp;
-            }
         }
+    return value;
+}
+
+function read_term(tokens){
+    var value = read_operand(tokens);
+    var operator = 0;
+    var temp = 0;
+    while(tokens.length >0){
+        if(tokens[0] == ')') return value;
+        operator = tokens.shift();
+        if(['+','-'].indexOf(operator) > -1){
+            tokens.unshift(operator);
+            break;
+        }
+        temp = read_operand(tokens);
+        if(operator === '*'){
+            value = value*temp;
+        }
+        if(operator === '/'){
+            value = value/temp;
+        }
+    }
     return value;
 }
 
