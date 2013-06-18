25aa8ef5fed71d45390f6d7205ed31100df2db91
laurabreiman <lauracle@mit.edu>
2013-06-18 10:25:27 -0400

    server stuff

---
+++ MITXServer/myserver.js
@@ -0,0 +1,14 @@
+var questions = [{"questionText": "What is Laura's favorite vegetable?", "options": ["Carrot", "Kale", "Broccoli", "Brussel Sprouts"], "solutionIndex": 2}, {"questionText": "What is Laura's favorite Pokemon?", "options": ["Beedrill", "Abra", "Pikachu", "Porygon"], "solutionIndex": 1}];
+var sys = require("sys"),  
+my_http = require("http"); 
+my_url = require('url');
+my_http.createServer(function(request,response){  
+    sys.puts("I got kicked"); 
+    var data = my_url.parse(request.url, true).query;
+    var currentQIn = parseInt(data.index);
+    var result = (questions[currentQIn].options[questions[currentQIn].solutionIndex] == data.answer);
+    response.writeHeader(200, {"Content-Type": "text/plain", 'Access-Control-Allow-Origin': '*'});  
+    response.write(""+result);  
+    response.end();  
+}).listen(8080);  
+sys.puts("Server Running on 8080");  
