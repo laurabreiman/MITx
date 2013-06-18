5f89ed451bcc97368f925784cb34d012b2a4a114
laurabreiman <lauracle@mit.edu>
2013-06-18 16:13:50 -0400

    Added greater functionality

--- bootstrap-prac/fluid-site.js
+++ bootstrap-prac/fluid-site.js
@@ -2,23 +2,12 @@
  * A simple fluid site built with Bootstrap
  */
 function setup(div){
-  var template = ""
-    + "<div class='navbar navbar-inverse navbar-fixed-top'>"
-    + "<div class='navbar-inner'>"
-    + "<a class='brand' href='#'>Example Page</a>"
-    + "<ul class='nav'>"
-    + "<li class='active'><a href='#'>Home</a></li>"
-    + "<li><a href='#'>About</a></li>"
-    + "<li><a href='#'>Contact</a></li>"
-    + "</ul>"
-    + "</div>"
-    + "</div>";
     
     var root = $("<div class='topbar'></div>");
     root.append(template);
+    $('.icon').popover('content');
 }
 
 $(document).ready(function() {
-    $('.fluidsite').each(function() {
-        setup(this);  
-    });
+    $(".icon").popover({trigger: 'hover', title: "It's all yours", content: "Don't things taste better when they're Jüst right?"});
+});
