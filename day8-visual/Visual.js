0f120d39d1ab8b7dab3ea2062441f1ad5a0a2291
laurabreiman <lauracle@mit.edu>
2013-06-19 10:34:35 -0400

    Switched to svg

--- day8-visual/Visual.js
+++ day8-visual/Visual.js
@@ -1,16 +1,11 @@
 var data = [0,4,8,8,15,16,23,42];
 
+var chart_height = 140;
 
-//var chart = $("<div class='chart'></div>");
-//$('.chart-container').append(chart);
-
-//data.forEach(function(d) {
-//    chart.append(d);
-//});
-
-//data.forEach(function(d) {
-//    chart.append($("<div></div").css('width',d*10+"px").text(d));
-//});
 var x_scale = d3.scale.linear().domain([0,d3.max(data)]).range(["0%","100%"]);
-var chart = d3.select(".chart-container").append("div").attr("class","chart");
-chart.selectAll('div').data(data).enter().append('div').style("width",x_scale).text(function(d){return d});
+var y_scale = d3.scale.ordinal().domain(d3.keys(data)).rangeBands([0,chart_height]);
+
+var chart = d3.select(".chart-container").append("svg").attr("class","chart");
+chart.selectAll('rect').data(data).enter().append('rect').attr("y",function(d,i){return y_scale(i);}).attr("width",x_scale).attr("height",20);
+
+chart.selectAll("text").data(data).enter().append("text").attr("x",x_scale).attr("y", function(d,i){return y_scale(i) + y_scale.rangeBand()/2;}).attr("dx",-3).attr("dy","0.35em").attr("text-anchor","end").text(function(d){return d;});
